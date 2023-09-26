26712f455707031162aad2959c1a10bce544155c
Blake Regalia <blake.regalia@gmail.com>
2023-09-26 07:11:40 -0700

    fix: secret app types

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ export * from './protobuf-reader';
 export * from './protobuf-writer';
 export * from './ripemd160';
 export * from './secp256k1';
+export * from './secret-app';
 export * from './secret-contract';
 export * from './secret-wasm';
 export * from './snip-52';
--- src/secret-app.ts
+++ src/secret-app.ts
@@ -1,8 +1,11 @@
+import type {CreateQueryArgsAndAuthParams} from './inferencing';
 import type {SecretContract} from './secret-contract';
 import type {AuthSecret, SlimCoin} from './types';
 import type {Wallet} from './wallet';
 
 import type {SecretAccAddr} from '@solar-republic/contractor/datatypes';
+
+import type {ContractInterface} from '@solar-republic/contractor/typings';
 
 import {__UNDEFINED, type JsonObject} from '@blake.regalia/belt';
 
@@ -12,6 +15,12 @@ export const exec_fees = (xg_limit: bigint|`${bigint}`, x_gas_price: number, s_d
 	''+Math.ceil(Number(xg_limit) * x_gas_price), s_denom],
 ] as [SlimCoin];
 
+type ConvertAuth<a_tuple extends [any?, any?]> = a_tuple extends [any, any]
+	? [a_tuple[0], 1]
+	: undefined extends a_tuple[1]
+		? [a_tuple[0], 0?]
+		: [a_tuple[0], 1];
+
 /**
  * Simple wrapper for querying and executing a Secret contract. Binds Wallet and SecretContract, as well
  * as an AuthSecret, gas price, and optional granter.
@@ -19,11 +28,30 @@ export const exec_fees = (xg_limit: bigint|`${bigint}`, x_gas_price: number, s_d
 export interface SecretApp {
 	price(xn_price: number): void;
 
-	query(
-		si_method: string,
-		h_args: JsonObject,
-		xc_auth: 0|1,
-	): ReturnType<typeof query_contract_infer>;
+	// query(
+	// 	si_method: string,
+	// 	h_args: JsonObject,
+	// 	xc_auth: 0|1,
+	// ): ReturnType<typeof query_contract_infer>;
+
+	query<
+		g_interface extends ContractInterface,
+		h_variants extends ContractInterface.MsgAndAnswer<g_interface, 'queries'>,
+		si_method extends Extract<keyof h_variants, string>,
+		g_variant extends h_variants[si_method],
+	>(
+		si_method: si_method,
+		...[h_args, xc_auth]: ConvertAuth<CreateQueryArgsAndAuthParams<
+			h_variants,
+			si_method,
+			ContractInterface extends g_interface? 1: 0
+		>>
+	): Promise<[
+		w_result: g_variant['response'] | undefined,
+		xc_code_x: number,
+		s_error: string,
+		h_answer?: g_variant['answer'],
+	]>;
 
 	exec(
 		h_exec: JsonObject,
